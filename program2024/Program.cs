1c0bcd5f70e4bb876d9d0215b1368c309d51d3da
Ruben <anda94@live.no>
2024-05-09 03:53:19 +0200

    Add project files.

---
+++ program2024/Program.cs
@@ -0,0 +1,17 @@
+﻿using program2024;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        string? name = MyConsole.Ask("hva heter du ?");
+        Console.WriteLine($"Hei, {name}!");
+
+        int? birthYear = MyConsole.AskForInt("Hvilket år er du født ?");
+        var hadBirthday = MyConsole.AskForBool("Har du hatt bursdag i år ?");
+        var age = 2024 - birthYear;
+        if (!hadBirthday) age--;
+        Console.WriteLine($"Da er du {age} år gammel!");
+        Console.WriteLine($"Da er du " + age + " år gammel!");
+    }
+}
---
+++ program2024/myConsole.cs
@@ -0,0 +1,23 @@
+﻿namespace program2024
+{
+    internal class MyConsole
+    {
+        public static string Ask(string qustion)
+        {
+            Console.Write(qustion + " ");
+            return Console.ReadLine();
+        }
+        public static int AskForInt(string qustion)
+        {
+
+            var answearStr = Ask(qustion);
+            return Convert.ToInt32(answearStr);
+        }
+        public static bool AskForBool(string qustion)
+        {
+
+            var answearStr = Ask(qustion + " (y/n)");
+            return answearStr.ToLower().StartsWith("y");
+        }
+    }
+}
